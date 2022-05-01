1e1d054221384142015482d70777491ebc22ac41
iwillywonka94 <iwillywonka94@gmail.com>
2022-05-01 20:47:53 +0300

    add fetchCounties

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+const API_URL = `https://restcountries.com/v3.1/name/`
+export function fetchCountries (name) {
+    return fetch(`${API_URL}${name}?fields=name,capital,population,flags,languages`).then(response => {
+        if (!response.ok) {
+            throw Error(response.status);
+        }
+        return response.json
+    })
+}
