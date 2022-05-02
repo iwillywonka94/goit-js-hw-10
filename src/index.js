203bd4b2055144ed16b7a517ec854cf6831aec2e
iwillywonka94 <iwillywonka94@gmail.com>
2022-05-02 19:18:15 +0300

    i hope its finish work :airplane:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-import {fetchCountries} from './fetchCountries';
+import { fetchCountries } from './scripts/fetchCountries';
+import { input, countryList, countryInfo } from './scripts/refs'
+
 
 const DEBOUNCE_DELAY = 300;
-console.log()
+const MANY_SEARCH_RESULTS = "Too many matches found. Please enter a more specific name.";
+const ERROR_MESSAGE = "Oops, there is no country with that name";
+
+input.addEventListener("input", debounce(searchField, DEBOUNCE_DELAY))
+
+function searchField (value) {
+    if (!input.value) {
+        clearInput();
+        return
+    }
+    fetchCountries(input.value.trim()).then(response => {
+        if(response.length >= 10) {
+            return Notiflix.Notify.failure(MANY_SEARCH_RESULTS);
+        }
+        else if (response.length >= 2 && response.length < 10) {
+            clearInput();
+            markupListCountry(response);
+        }
+        else {
+            clearInput();
+            markupInfoAboutCountry(response);
+        }
+    }).catch(error => {
+        clearInput();
+        console.error(error);
+        Notiflix.Notify.failure(ERROR_MESSAGE);
+    })
+};
+
+
+function clearInput () {
+    countryList.innerHTML = "";
+    countryInfo.innerHTML = "";
+};
+
+function markupListCountry(array) {
+    const markUp = array.map(({name, flags}) => `<li class="list"><img width="50" src="${flags.svg}"> ${name.official}</li>`).join("")
+    countryList.insertAdjacentHTML('beforeend', markUp)
+};
+
+function markupInfoAboutCountry (array) {
+    const markUp = array.map(({name, capital, population, languages, flags}) => `<ul><li class="list"><img width="50" src="${flags.svg}"> ${name.official}</li><li class="item">Capital: ${capital}</li><li class="item">Population: ${population}</li><li class="item">Languages: ${Object.values(languages)}</li></ul>`).join("")
+    countryInfo.insertAdjacentHTML('beforeend', markUp);
+};
---
+++ src/scripts/refs.js
@@ -0,0 +1,5 @@
+const input = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+export {input, countryList, countryInfo};
